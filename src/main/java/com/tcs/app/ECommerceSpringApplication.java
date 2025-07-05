be2b0ac3f64d9989256bb3b97ec20be73b48d608
Mustafa9164 <mohdmustafa9164@gmail.com>
2025-07-05 09:31:59 +0530

    intial commit

---
+++ src/main/java/com/tcs/app/ECommerceSpringApplication.java
@@ -0,0 +1,14 @@
+package com.tcs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(ECommerceSpringApplication.class, args);
+	}
+
+}
