62e352d295a21a809eb062466165effb2074627c
Mustafa9164 <mohdmustafa9164@gmail.com>
2025-07-05 10:08:12 +0530

    added .env file

--- src/main/java/com/tcs/app/ECommerceSpringApplication.java
+++ src/main/java/com/tcs/app/ECommerceSpringApplication.java
@@ -1,5 +1,6 @@
 package com.tcs.app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ECommerceSpringApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+
+		Dotenv dotenv=Dotenv.configure().load();
+
+		dotenv.entries().forEach(entry->System.setProperty(entry.getKey(),entry.getValue()));
 		SpringApplication.run(ECommerceSpringApplication.class, args);
+
 	}
 
 }
